944564dca083961ed76a46413f3adff3d9f00446
LMHartley <liamhartley321@live.co.uk>
2019-06-24 15:30:11 +0100

    Completed subtract feature.

--- src/main/java/com/qa/calculator/Calculator.java
+++ src/main/java/com/qa/calculator/Calculator.java
@@ -5,5 +5,9 @@ public class Calculator {
 	public int add(int firstNumber, int secondNumber) {
 		return firstNumber+secondNumber;
 	}
+
+	public int subtract(int firstNumber, int secondNumber) {
+		return firstNumber-secondNumber;
+	}
 	
 }
--- src/test/java/com/qa/calculator/calculatorTest.java
+++ src/test/java/com/qa/calculator/calculatorTest.java
@@ -11,5 +11,11 @@ public class calculatorTest {
 		int result = calculator.add(2,2);
 		Assert.assertEquals(4, result);
 	}
+	@Test
+	public void subtractTwoNumbersTogetherTest() {
+		Calculator calculator = new Calculator();
+		int result = calculator.subtract(4,2);
+		Assert.assertEquals(2, result);
+	}
 
 }
